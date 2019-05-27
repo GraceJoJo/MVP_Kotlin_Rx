b172275f7663b088257709333be19530ab4b4570
zhoujuan <18510829974@163.com>
2019-05-27 22:27:58 +0800

    调试填充渐变色的曲线图的渐变颜色

--- learn/src/main/java/com/example/jojo/learn/MainActivity.kt
+++ learn/src/main/java/com/example/jojo/learn/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         var mCurrentStep: Float = 9709f
         sportstepview.startCountStep(mCurrentStep)
 
-        var datas = listOf<Int>(10, 66, 30, 25, 18, 2)
+        var datas = listOf<Int>(24, 20, 22, 15, 10, 8)
         var xList = listOf<String>("1月份", "2月份", "3月份", "4月份", "5月份", "6月份")
 
         //根据数据的最大值生成上下对应的Y轴坐标范围
@@ -35,15 +36,17 @@ class MainActivity : AppCompatActivity() {
         }
         barchartview.updateValueData(datas, xList, ylist)
         linechartview.updateData(datas, xList, ylist)
+        //测试绘制填充渐变色的曲线图
+        linechartshadowview.updateData(datas, xList, ylist)
 
         //  添加的是百分比
         var rateList = listOf<Float>(40f, 10f, 5f, 45f)
         // 添加的颜色
         var colors = mutableListOf<Int>()
-        colors.add(ContextCompat.getColor(this,R.color.color_55c7f2))
-        colors.add(ContextCompat.getColor(this,R.color.color_34617e))
-        colors.add(ContextCompat.getColor(this,R.color.color_7c80fe))
-        colors.add(ContextCompat.getColor(this,R.color.color_f1b133))
+        colors.add(ContextCompat.getColor(this, R.color.color_55c7f2))
+        colors.add(ContextCompat.getColor(this, R.color.color_34617e))
+        colors.add(ContextCompat.getColor(this, R.color.color_7c80fe))
+        colors.add(ContextCompat.getColor(this, R.color.color_f1b133))
         var showTextList = mutableListOf<String>()
         for (i in 0..rateList.size - 1) {
             showTextList.add(rateList.get(i).toString() + "%")
@@ -52,9 +55,31 @@ class MainActivity : AppCompatActivity() {
         piechartview.setShow(colors, rateList, true, true)
         //ViewCompat.animate(barchartview).scaleX(0.5f).scaleY(0.5f).setDuration(0).start()
 //        var rate = listOf<Float>(15f/100, 35f/100, 30f/100, 20f/100)
-        var rate = listOf<Float>(25f/100, 25f/100, 25f/100, 25f/100)
-        pieview.updateDate(rate,colors,true)
-        pieoview.updateDate(rate,colors,false)
+        var rate = listOf<Float>(25f / 100, 25f / 100, 25f / 100, 25f / 100)
+        pieview.updateDate(rate, colors, true)
+        pieoview.updateDate(rate, colors, false)
+
+
+
+        //测试带渐变填充色的曲线图
+        var timeList = ArrayList<Int>()
+        timeList.add(30)
+        timeList.add(20)
+        timeList.add(22)
+        timeList.add(8)
+        timeList.add(4)
+        timeList.add(0)
+        timeList.add(0)
+        var bottomList = ArrayList<String>()
+        bottomList.add("5.22")
+        bottomList.add("5.23")
+        bottomList.add("5.24")
+        bottomList.add("5.25")
+        bottomList.add("5.26")
+        bottomList.add("5.27")
+        bottomList.add("5.28")
+        studybendline.updateTime(timeList, bottomList )
+
     }
 
     //    public void textLayoutLeft(View v) {
@@ -63,4 +88,6 @@ class MainActivity : AppCompatActivity() {
     //    public void textLayoutCenter(View v) {
     //        mytextview.reLayoutText(1);
     //    }
+
+
 }
